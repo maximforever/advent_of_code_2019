25b8cfab251d0f60cacd249a25fada8ef32d3c05
Max Pekarsky <max@Maxs-MacBook-Pro.local>
2019-12-07 17:57:54 -0700

    day 4: get working password guesser

---
+++ day-4/password_guesser.rb
@@ -0,0 +1,59 @@
+class PasswordGuesser
+  def initialize(min, max)
+    @min = min
+    @max = max
+  end
+
+  def possible_numbers
+    possible_numbers = [].tap do |eligible_numbers|
+      for num in @min..@max do 
+        number = Number.new(num)
+        eligible_numbers << num if number.eligible?
+      end
+    end
+
+    return possible_numbers.length
+  end
+end
+
+class Number
+  def initialize(number)
+    @number = number
+  end
+
+  def has_increasing_digits?
+    previous_digit = 0
+
+    digit_array.each do |digit|
+      return false if digit < previous_digit
+      previous_digit = digit
+    end
+
+    true
+  end
+
+  def digit_array
+    @number.to_s.chars.map(&:to_i)
+  end
+
+  def has_two_adjacent_digits?
+    previous_digit = 0
+
+    digit_array.each do |digit|
+      return true if digit == previous_digit
+      previous_digit = digit
+    end
+
+    false
+  end
+
+  def has_6_digits?
+    return digit_array.length == 6
+  end
+
+  def eligible?
+    has_6_digits? && has_increasing_digits? && has_two_adjacent_digits?
+  end
+end
+
+p = PasswordGuesser.new(402328, 864247).possible_numbers
